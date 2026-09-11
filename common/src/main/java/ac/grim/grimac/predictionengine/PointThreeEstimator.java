81e5274fb74276ed6e20004ba7899c1bf84e5163
Pantera <89838384+Pantera07@users.noreply.github.com>
2026-09-12 04:56:39 +0900

    Fix method call to get check manager

--- common/src/main/java/ac/grim/grimac/predictionengine/PointThreeEstimator.java
+++ common/src/main/java/ac/grim/grimac/predictionengine/PointThreeEstimator.java
@@ -159,7 +159,7 @@ public class PointThreeEstimator {
             isNearFluid = true;
         }
 
-        player.checkManager.getCheck(Reach.class).handleBlockChange(new Vector3i(x, y, z), state);
+        player.checkManager.get(Reach.class).handleBlockChange(new Vector3i(x, y, z), state);
 
         if (pointThreeBox.isIntersected(new SimpleCollisionBox(x, y, z))) {
             // https://github.com/MWHunter/Grim/issues/613
